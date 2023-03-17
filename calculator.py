7199bf70489092791ea122cb227789a60efdf00d
Tressie Muthuri <tressiemuthuri@gmail.com>
2023-03-17 22:45:16 +0300

    Float

--- calculator.py
+++ calculator.py
@@ -1,3 +1,8 @@
-x = int (input ("What's x?"))
-y = int (input ("What's y?"))
-print (x+y)
+# x = int (input ("What's x?"))
+# y = int (input ("What's y?"))
+# print (x+y)
+
+x = float(input ("What's x? "))
+y = float(input ("What's y? "))
+print (f"{round (x + y):,}")
+# print (z)
