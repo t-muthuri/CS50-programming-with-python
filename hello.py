63d6411a28421a2420ba09c139d1656bcf22ce22
Tressie Muthuri <tressiemuthuri@gmail.com>
2023-03-17 16:55:41 +0300

    Functions and variables

---
+++ hello.py
@@ -0,0 +1,11 @@
+#ask the person for their name
+name = input ( "What's your name? ")
+
+#say hello to the person
+print ( "Hello,", name )
+
+"""
+why we use comments:
+remind the team what the code does and if you have achieved your goal
+could be used as pseudocode
+"""
