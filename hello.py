3e8bdc8fef0297e5db2d88b0d255593ece6ee18c
Tressie Muthuri <tressiemuthuri@gmail.com>
2023-03-17 19:55:46 +0300

    Documentation

--- hello.py
+++ hello.py
@@ -2,10 +2,18 @@
 name = input ( "What's your name? ")
 
 #say hello to the person
-print ( "Hello,", name )
+# print ( "Hello, ", end="")
+print ( "Hello,", name, sep=" ")
+
+# print (name)
+# print(*objects, sep=' ', end='\n', file=None, flush=False)
 
 """
 why we use comments:
 remind the team what the code does and if you have achieved your goal
 could be used as pseudocode
+
+Python documentation docs.python.org
+
+Learn to read the documentation when learning a new program
 """
