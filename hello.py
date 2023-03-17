b4a129dd90b51be14bc84b9750a79de61206ce1a
Tressie Muthuri <tressiemuthuri@gmail.com>
2023-03-17 22:28:19 +0300

    Calculator

--- hello.py
+++ hello.py
@@ -1,21 +1,8 @@
-#ask the person for their name
-name = input ( "What's your name? ")
+#ask the person for their name and remove whitespace from str and capitalize person's name
+name = input ( "What's your name? ").strip().title()
+
+#split name into first and last name
+first, last = name.split(" ")
 
 #say hello to the person
-# print (f"Hello, {name}")
-# print ( "Hello, ", end="")
-# print ( "Hello,", name, sep=" ")
-
-# print (name)
-# print(*objects, sep=' ', end='\n', file=None, flush=False)
-
-
-"""
-why we use comments:
-remind the team what the code does and if you have achieved your goal
-could be used as pseudocode
-
-Python documentation docs.python.org
-
-Learn to read the documentation when learning a new program
-"""
+print (f"Hello, {first}")
