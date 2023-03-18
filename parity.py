7d8348e49c0d5c8e197663906c3cb49d9b1ad523
Tressie Muthuri <tressiemuthuri@gmail.com>
2023-03-18 08:04:54 +0300

    one line

--- parity.py
+++ parity.py
@@ -1,6 +1,18 @@
-x = (int(input("What's x? ")))
+# x = (int(input("What's x? ")))
 
-if x % 2 == 0:
-    print("Even")
-else:
-    print("Odd")
+# if x % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+def main ():
+    x = int(input("What's x? "))
+    if is_even(x):
+        print ("Even")
+    else:
+        print("Odd")
+
+def is_even(n):
+    return True if n % 2 == 0 else False
+
+main()
