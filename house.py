0652b10cfeb89b5b4b74fd471962a054824c99a5
Tressie Muthuri <tressiemuthuri@gmail.com>
2023-03-18 08:15:49 +0300

    readable

--- house.py
+++ house.py
@@ -1,8 +1,9 @@
 name = input("What's your name? ")
 
-if name == "Harry" or name == "Hermione" or name == "Ron":
-    print("Gryffindor")
-elif name == "Draco":
-    print("Slytherin")
-else:
-    print("Who dares enter?")
+match name:
+    case "Harry" | "Hermione" | "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who?")
