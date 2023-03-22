35cfba320efa66dd3a13a2d8077dd9c2d9b9dbfe
Tressie Muthuri <tressiemuthuri@gmail.com>
2023-03-22 13:58:43 +0300

    refactored

--- mario.py
+++ mario.py
@@ -1,13 +1,9 @@
 def main():
-    print_square(3)
+    print_square(int(input("What's n? ")))
 
 def print_square(size):
-    # for each row in square
     for i in range(size):
-        # for each brick in row
-        for j in range(size):
-            # print brick
-            print("#", end="")
+        print("#" * size, end="")
         print()
 
 main()
