317801c3e1177f2702c4c96fd1a83a25b583c3cb
t-muthuri <tressiemuthuri@gmail.com>
2023-05-05 21:26:48 +0300

    mutable

--- students3.py
+++ students3.py
@@ -1,11 +1,13 @@
 def main():
-    name, house = get_student()
-    print(f"{name} from {house}")
+    student = get_student()
+    if student[0] == "Padma":
+        student[1] = "Ravenclaw"
+    print(f"{student[0]} from {student[1]}")
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    return name, house
+    return [name, house]
 
 if __name__ == "__main__":
     main()
