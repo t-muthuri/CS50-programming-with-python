d9db9d7bc544e675db8153895127cbed9c9247d2
unknown <tressiemuthuri@gmail.com>
2023-05-05 19:53:39 +0300

    regex

---
+++ students.py
@@ -0,0 +1,9 @@
+import csv
+
+name = input("What's your name? ")
+home = input("Where's your home? ")
+house = input("What's your house? ")
+
+with open("students.csv", "a") as file:
+    writer = csv.DictWriter(file, fieldnames=["name", "home", "house"])
+    writer.writerow({"name": name, "home":home, "house": house})
