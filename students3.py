9e46c61707d8f349754bd78b49af58359747593b
t-muthuri <tressiemuthuri@gmail.com>
2023-05-06 07:53:27 +0300

    Methods in classes

--- students3.py
+++ students3.py
@@ -1,13 +1,19 @@
+class Student:
+    def __init__(self, name, house): #instance variables to objects
+        self.name = name
+        self.house = house
+    
+
 def main():
     student = get_student()
-    if student["name"] == "Padma":
-        student["house"] = "Ravenclaw"
-    print(f"{student['name']} from {student['house']}")
+    print(f"{student.name} from {student.house}")
+
 
 def get_student():
-    name = input("Name: ")
+    name = input ("Name: ")
     house = input("House: ")
-    return {"name": name, "house": house}
+    student = Student(name, house) #constructor call-instantiates a student object
+    return student
 
 if __name__ == "__main__":
     main()
