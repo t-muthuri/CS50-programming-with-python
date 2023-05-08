f8a12d2953870a150e9c5361f3ac37f24964dd6a
t-muthuri <tressiemuthuri@gmail.com>
2023-05-08 09:21:56 +0300

    Charm/Str

--- students3.py
+++ students3.py
@@ -1,24 +1,38 @@
 class Student:
-    def __init__(self, name, house): #instance variables to objects
+    def __init__(self, name, house, patronus): #instance variables to objects
         if not name:
             raise ValueError("Missing name")
         if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
             raise ValueError("Invalid house")
         self.name = name
         self.house = house
+        self.patronus = patronus
     
     def __str__(self):
         return f"{self.name} from {self.house}"
+    
+    def charm(self):
+        match self.patronus:
+            case "Stag":
+                return "H"
+            case "Otter":
+                return "O"
+            case "Jack Russell terrier":
+                return "JST"
+            case _:
+                return "/"
 
 def main():
     student = get_student()
-    print(student)
+    print("Expecto Patronum!")
+    print(student.charm())
 
 
 def get_student():
     name = input ("Name: ")
     house = input("House: ")
-    return Student(name, house)
+    patronus = input("Patronus: ")
+    return Student(name, house, patronus )
 
 if __name__ == "__main__":
     main()
