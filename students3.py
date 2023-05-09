f1e22b5b1fc8e9066d021525c49246d8fd0ef7d4
t-muthuri <tressiemuthuri@gmail.com>
2023-05-09 20:06:46 +0300

    Inheritance

--- students3.py
+++ students3.py
@@ -6,37 +6,17 @@ class Student:
     def __str__(self):
         return f"{self.name} from {self.house}"
     
-    @property
-    def name(self):
-        return self._name
-    
-    @name.setter
-    def name(self, name):
-        if not name:
-            raise ValueError("Missing name")
-        self._name = name
-        
-    @property
-    def house(self):
-        return self._house #underscore to differentiate between the instance variables and the setter
-    
-    #setter-function that sets a value
-    @house.setter #a decorator
-    def house(self, house):
-        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
-            raise ValueError("Invalid house")
-        self._house = house
+    @classmethod
+    def get(cls):
+        name = input("Name: ")
+        house = input("House: ")
+        return cls(name, house) #you can instatiate a student object by calling cls
 
 
 def main():
-    student = get_student()
+    student = Student.get() #
     print(student)
 
-
-def get_student():
-    name = input ("Name: ")
-    house = input("House: ")
-    return Student(name, house )
 
 if __name__ == "__main__":
     main()
