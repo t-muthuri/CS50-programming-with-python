9e86aebd71424a712588859f48261f7fb6d8f734
t-muthuri <tressiemuthuri@gmail.com>
2023-05-10 12:40:51 +0300

    -n cli

--- meows.py
+++ meows.py
@@ -1,16 +1,10 @@
-def meow(n: int) -> str:
-    """
-    Meow n times.
+import sys
 
-    :param n: Number of times to meow
-    :type n: int
-    :raise TypeError: If n is not an int
-    :return: A string of n meows, one per line
-    :rtype: str
-    """
-    return "meow\n" * n
-
-
-number: int = int(input("Number: "))
-meows: str = meow(number)
-print(meows, end="")
+if len(sys.argv) == 1:
+    print("meow")
+elif len(sys.argv) == 3 and sys.argv[1] == "-n":
+    n = int(sys.argv[2])
+    for _ in range(n):
+        print("meow")
+else:
+    print("usage: meows.py")
