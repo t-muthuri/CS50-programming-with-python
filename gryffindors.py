b3b1b1deaf5ce99bd487b15f6b22329182460f9e
t-muthuri <tressiemuthuri@gmail.com>
2023-05-13 09:03:43 +0300

    dictionary comprehensions

--- gryffindors.py
+++ gryffindors.py
@@ -1,5 +1,5 @@
 students = ["Hermione", "Harry", "Ron"]
 
-gryffindors = [{"name": student, "house": "Gryffindor"} for student in students]
+gryffindors = {student: "Gryffindor" for student in students}
 
 print(gryffindors)
